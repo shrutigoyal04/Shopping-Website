a273f93da2fb3a5a34bdf5884a8643456a9b104e
shrutigoyal04 <goyalshruti016@gmail.com>
2024-08-30 15:57:13 +0530

    shopping website

---
+++ furniture backend/conn.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+mongoose.connect(process.env.URL);
+mongoose.connection.on('connected', () => console.log('connected'));
+
+const { Schema } = mongoose;
+const contactSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    message: String
+});
+const Contact = mongoose.model('Contact', contactSchema);
+
+const orderSchema = new Schema(
+    {
+        formdata: {
+          country: String,
+          firstname: String,
+          lastname: String,
+          companyname: String,
+          address: String,
+          state: String,
+          zip: String,
+          email: String,
+          phoneno: String,
+          notes: String
+        },
+        cartdata: [
+          {
+            id: Number,
+            productImage: String,
+            productName: String,
+            productPrice: Number,
+            pQuantity: Number
+          }
+        ]
+      }
+)
+const Order = mongoose.model('Order',orderSchema)
+module.exports = {Contact,Order}
+
+
+
+
