06605180577cdc9e17d31e987dd3494a318c82ac
shrutigoyal04 <goyalshruti016@gmail.com>
2024-09-02 15:49:06 +0530

    changes

--- furniture fronted/src/Components/Hero.jsx
+++ furniture fronted/src/Components/Hero.jsx
@@ -11,12 +11,12 @@ function Hero({ two }) {
             <div className="col-lg-5">
               <div className="intro-excerpt">
                 <h1>{two["title"]}</h1>
-                <p className="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</p>
+                <p className="mb-4">Furniture Website . Explore more!!</p>
                 <p>
-                  <a href="" className="btn btn-secondary me-2">
+                  <a href="shop" className="btn btn-secondary me-2">
                     Shop Now
                   </a>
-                  <a href="#" className="btn btn-white-outline">
+                  <a href="shop" className="btn btn-white-outline">
                     Explore
                   </a>
                 </p>
